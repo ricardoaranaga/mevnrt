ef678cd0f666e262e384b1487891dfd869a96698
Ricardo Aranaga <ricardo.aranaga@gmail.com>
2025-12-14 22:27:14 -0400

    fix intial admin creation bug

--- api-server/helpers/db/mongodb.js
+++ api-server/helpers/db/mongodb.js
@@ -20,63 +20,35 @@ const init = () => {
   });
 };
 
-function initial() {
-  Role.estimatedDocumentCount((err, count) => {
-    if (!err && count === 0) {
-      new Role({
-        name: "user"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-        console.log("added 'user' to roles collection");
-      });
-      new Role({
-        name: "moderator"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-        console.log("added 'moderator' to roles collection");
-      });
-      new Role({
-        name: "admin"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-        console.log("added 'admin' to roles collection");
-      });
+async function initial() {
+  try {
+    const roleCount = await Role.estimatedDocumentCount();
+    if (roleCount === 0) {
+      await Role.create([
+        { name: "user" },
+        { name: "moderator" },
+        { name: "admin" }
+      ]);
+      console.log("Added 'user', 'moderator', 'admin' to roles collection");
     }
-  });
-  User.estimatedDocumentCount((err, count) => {
-    if (!err && count === 0) {
-      new User({
+
+    const userCount = await User.estimatedDocumentCount();
+    if (userCount === 0) {
+      const adminRole = await Role.findOne({ name: "admin" });
+      const admin = new User({
         username: 'admin',
         name: 'Admin',
         lastname: 'Admin',
         email: 'example@email.com',
-        password: bcrypt.hashSync('12345678', 8)    
-      }).save((err, user) => {
-        if (err) {
-          console.log("error", err);
-        }
-        Role.findOne({ name: "admin" }, (err, role) => {
-          if (err) {
-            console.log("Error in Init: Role", err)
-          }
-          user.roles = [role._id];
-          user.save(err => {
-            if (err) {
-              console.log("Error in Init: Save User", err)
-            }
-            console.log("User init: Saved successfully");
-          });
-        });
-        console.log("added 'admin' to users collection");
+        password: bcrypt.hashSync('12345678', 8)
       });
+      admin.roles = [adminRole._id];
+      await admin.save();
+      console.log("Added 'admin' to users collection");
     }
-  });
+  } catch (err) {
+    console.error("Initial setup failed:", err);
+  }
 }
 
 mongoose.Promise = global.Promise;
